86c3c317ba86d3cd0f8c97b234a8f008428fe6bd
Jamie Duncan <djamie@vmware.com>
2020-04-21 23:40:52 -0400

    probably not functional right now. working on bringing in logging and starting to catch exceptions. Also working on sending tweets and settling on a config file set of parameters.

--- pychicken/pychicken.py
+++ pychicken/pychicken.py
@@ -3,45 +3,103 @@ from signal import pause
 from datetime import datetime
 from time import sleep
 from random import randrange
+import configparser
+import csv
+import requests
+import logging
 import tweepy
 from gpiozero import MotionSensor
 from picamera import PiCamera
-
-motion_sensor_pin = 14
 
 class pyChicken:
   """Python3 application for Raspberry Pis to take advantage of an onboard camera and a PIR motion sensor to automate backyard foul social media
   """
 
-  def __init__(self, motion_sensor_pin=motion_sensor_pin):
-    self.timestamp = self._set_timestamp()
-    self.tweet = True
-    self.tweet_interval = (60 * 60) # max 1 tweet per hour
-    self.image_filename = 'tweetpic.jpg'
-    self.image_path = '/home/pi'
-    self.twitter_image = os.path.join(
-      self.image_path,
-      self.image_filename
-    )
-    self.running_livestream = False
-    self.livestreams = False
-    self.motion_sensor_pin = motion_sensor_pin
+  def __init__(self, options):
 
-    self.camera = PiCamera()
-    self.camera.resolution = (1024, 768)
-    print("Initializing Camera")
-    sleep(2)
+    self.logger = logging.getLogger()
+    
+    # grab the config file and read in the options
+    self.logger.debug("Processing __init__.py")
+    self.config = configparser.ConfigParser()
+    self.config.read(options.config)
 
+    # There's a motion sensor installed and we want to use it via the GPIO pins
+    self.use_motion_sensor = self.config['motion_sensor']['enabled']
+    if self.use_motion_sensor:
+      self.logger.debug("Setting motion sensor pin number")
+      self.motion_sensor_pin = self.config['motion_sensor']['gpio_pin']
+
+    # We're going to be using a camera attached to our Raspberry Pi Note: this
+    # is written for direct-connected cameras, not USB cameras as of right now
+    self.use_camera = self.config['camera']['enabled']
+    if self.use_camera:
+      self.logger.info("Initializing camera")
+      self.camera = PiCamera()
+      self.camera.resolution = (1024, 768)
+      sleep(2)
+
+    # check to see if we're going to be sending tweets
+    self.send_tweets = self.config['twitter']['enabled']
+
+    # If we're going to be sending out tweets we need to set up access through
+    # the Twitter API
+    if self.send_tweets:
+      self.twitter = self._create_twitter_api()
+      self._setup_twitter_params() 
+
+    self.send_livestream = self.config['livestream']['send_livestream']
+  
     self.chicken_facts = list()
     self.chicken_facts_count = len(self.chicken_facts)
+    self.timestamp = self._set_timestamp()
+
+  def _setup_twitter_params(self):
+    """ Sets up some needed parameterss to properly process information and send it to Twitter.
+    """
+    
+    self.tweet_interval = self.config['twitter']['tweet_interval']
+    if self.use_camera:
+      self.logger.info("Seetting up media information for Twitter")
+      self.image_filename = 'tweetpic.jpg'
+      self.image_path = '/home/pi'
+      self.twitter_image = os.path.join(
+                         self.image_path,
+                         self.image_filename
+      )
+
+  def _create_twitter_api(self):
+    """sets up and confirms the Twitter API is functional. Returns the twitter API object for use in other functions.
+    """
+
+    consumer_key = self.config['twitter']['consumer_key']
+    consumer_secret = self.config['twitter']['consumer_secret']
+    access_token = self.config['twitter']['access_token']
+    access_token_secret = self.config['twitter']['access_token_secret']
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+    api = tweepy.API(auth,
+                    wait_on_rate_limit=True,
+                    wait_on_rate_limit_notify=True)
+
+    try:
+      api.verify_credentials()
+    except Exception as e:
+      self.logger.error("Error verifying Twitter API", exc_info=True)
+      raise e
+    self.logger.info("Twitter API connected and verified")
+
+    return api
 
   def _set_timestamp(self):
     """ We don't want a chicken walking around all the time to cause a twitter storm. So we'll set a timestamp and use it for comparison so we don't send out too many pictures.
     """
     
-    self.timestamp = datetime.now()
+    timestamp = datetime.now()
 
-    return True
+    return timestamp
     
   def _check_timestamp(self):
     """ Checks to see if it's OK to send out a tweet or if we should wait. Uses self.timestamp_interval as the minimum wait between events
@@ -63,21 +121,32 @@ class pyChicken:
     """
     
     # we don't want to try to capture a pic if we're running a livestream
-    if not self.running_livestream: 
-      self.camera.start_preview()
-      sleep(2)
+    if self.running_livestream:
+      self.logger.info("Livestream running. Not going to capture image")
+      return False
+    else:
+      try:
+        self.logger.info("Preparing to capture image")
+        self.camera.start_preview()
+        sleep(2)
 
-      self.camera.capture(self.twitter_image)
+        self.camera.capture(self.twitter_image)
+        return False
+
+      except Exception as e:
+        self.logger.error("Unable to capture image.", exc_info=True)
+        raise e
 
   def _send_tweet(self, message, attach_pic=True):
     """ Takes a still picture that was just taken and sends out a tweet with the picture and some pre-defined text
     """
     if self.running_livestream:
-      message = self.livestream_message
+      self.twitter_api.update_status(self.livestream_message)
     else:
       message = self._get_tweet_quote()
+      status = self.twitter_api.update_with_media(self.twitter_image, message)
 
-    pass
+      self.twitter_api.update_status(status = message)
 
   def _get_tweet_fact(self):
     """ Grabs a random fact about chickens to attach to a tweet that is being sent out
@@ -113,8 +182,8 @@ class pyChicken:
 
   def _load_facts_file(self, facts_file):
     """ Takes a CSV fie in the format:
-    <fact_type>,<fact_content>,<fact_author>
-    and loads it into the database for use when sending out tweets.
+    <fact_type>,<fact_content>,<fact_author> and loads it into the database for
+    use when sending out tweets.
     """
 
   def _add_tweet_quote(self, quote, fact_type='fact', author=None):
@@ -134,7 +203,7 @@ class pyChicken:
     print("capturing image at {p}".format(p=self.twitter_image))
     self._image_capture()
 
-  def run(self, options):
+  def run(self):
     """ The primary function. This is called by a script, loads a CSV file full of facts to use as social media content, and begins checking for the motion sensor, start livestreams, etc.
     """
 
