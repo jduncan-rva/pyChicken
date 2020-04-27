bd6e6e3b9c4806b13c80404913b1b0ceb3d7aaff
Jamie Duncan <djamie@vmware.com>
2020-04-27 17:02:25 -0400

    working on livestream

--- pychicken/pychicken.py
+++ pychicken/pychicken.py
@@ -1,17 +1,21 @@
 import os
+import io
+import shutil
 from signal import pause
 from datetime import datetime
-from time import sleep
+from time import sleep, time
 from random import randrange
+from http import server
+import socketserver
 import configparser
 import requests
 import yaml
 import logging
 import threading
-import subprocess
 import tweepy
 from gpiozero import MotionSensor
 from picamera import PiCamera
+
 
 class pyChicken:
   """Python3 application for Raspberry Pis to take advantage of an onboard camera and a PIR motion sensor to automate backyard foul social media
@@ -21,14 +25,14 @@ class pyChicken:
 
     # Grab the config file and read in the options
     logging.basicConfig(filename='pychicken.log',
-                      level=logging.INFO,
-                      format='%(asctime)s %(name)s %(levelname)s: %(message)s',
-                      datefmt='%m-%d %H:%M:%S')
+          level=logging.INFO,
+          format='%(asctime)s %(name)s %(levelname)s: %(message)s',
+          datefmt='%m-%d %H:%M:%S')
     logging.debug("Processing __init__.py")
     config = configparser.ConfigParser()
     config.read(options.config)
 
-    # Get state of facts engine 
+    # Get state of facts engine
     use_facts = config['facts']['enabled']
     # There's a motion sensor installed and we want to use it via the GPIO pins
     use_motion_sensor = config['motion_sensor']['enabled']
@@ -42,15 +46,25 @@ class pyChicken:
 
     if send_livestream:
       self.running_livestream = False
-      self.youtube_key = config['livestream']['youtube_key']
-      self.youtube_url = config['livestream']['youtube_url']
-      self.youtube_feed_url = config['livestream']['youtube_feed_url']
       self.livestream_duration = config['livestream']['duration']
-    
+      self.http_port = config['livestream']['http_port']
+      self.ws_port = config['livestream']['ws_port']
+      self.page = """\
+        < html >
+        <head >
+        <title > picamera MJPEG streaming demo < /title >
+        </head >
+        <body >
+        <h1 > PiCamera MJPEG Streaming Demo < /h1 >
+        <img src = "stream.mjpg" width = "640" height = "480" / >
+        </body >
+        </html >
+        """
+
     if use_facts:
       self.facts_url = config['facts']['facts_url']
       self.facts, self.facts_count = self._load_facts_file()
-    
+
     if use_motion_sensor:
       logging.info("Setting motion sensor pin number")
       self.motion_sensor_pin = config['motion_sensor']['gpio_pin']
@@ -58,6 +72,11 @@ class pyChicken:
     if use_camera:
       if config['camera']['text']:
         self.camera_text = config['camera']['text']
+        self.vflip = config['camera']['vflip']
+        self.hflip = config['camera']['hflip']
+        self.width = config['camera']['width']
+        self.height = config['camera']['height']
+        self.framerate = config['camera']['framerate']
       else:
         self.camera_text = False
 
@@ -72,10 +91,10 @@ class pyChicken:
       access_token_secret = config['twitter']['access_token_secret']
 
       self.twitter = self._create_twitter_api(key=consumer_key,
-                     secret=consumer_secret,
-                     token=access_token,
-                     token_secret=access_token_secret,
-                     use_camera=use_camera)
+                secret=consumer_secret,
+                token=access_token,
+                token_secret=access_token_secret,
+                use_camera=use_camera)
 
     self.timestamp = self._set_timestamp()
 
@@ -85,11 +104,11 @@ class pyChicken:
     try:
       logging.info("Initializing camera")
       self.camera = PiCamera()
-      self.camera.resolution = (1024, 768)
-      self.camera.framerate = 25
+      self.camera.resolution = (self.width, self.height)
+      self.camera.framerate = self.framerate
       if self.camera_text:
         self.camera.annotate_text = self.camera_text
-      sleep(2)
+        sleep(2)
 
       return True
 
@@ -103,7 +122,7 @@ class pyChicken:
     try:
       logging.info("Closing camera interface")
       self.camera.close()
-    
+
     except Exception as e:
       logging.error("Unable to close camera", exc_info=True)
       raise e
@@ -116,8 +135,8 @@ class pyChicken:
     auth.set_access_token(token, token_secret)
 
     api = tweepy.API(auth,
-                    wait_on_rate_limit=True,
-                    wait_on_rate_limit_notify=True)
+        wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True)
 
     try:
       api.verify_credentials()
@@ -131,8 +150,8 @@ class pyChicken:
       self.image_filename = 'tweetpic.jpg'
       self.image_path = '/home/pi'
       self.twitter_image = os.path.join(
-                         self.image_path,
-                         self.image_filename
+      self.image_path,
+      self.image_filename
       )
 
     return api
@@ -140,11 +159,11 @@ class pyChicken:
   def _set_timestamp(self):
     """ We don't want a chicken walking around all the time to cause a twitter storm. So we'll set a timestamp and use it for comparison so we don't send out too many pictures.
     """
-    
+
     timestamp = datetime.now()
 
     return timestamp
-    
+
   def _check_timestamp(self):
     """ Checks to see if it's OK to send out a tweet or if we should wait. Uses self.timestamp_interval as the minimum wait between events
     """
@@ -153,7 +172,7 @@ class pyChicken:
     time_since_tweet = curr_time - self.timestamp
     secs_since_tweet = time_since_tweet.total_seconds()
 
-    if ( secs_since_tweet > self.tweet_interval ):
+    if (secs_since_tweet > self.tweet_interval):
       self.timestamp = self._set_timestamp()
       return True
 
@@ -163,7 +182,7 @@ class pyChicken:
   def _image_capture(self):
     """ Captures a still image and save it to a file for uplaoding to a tweet
     """
-    
+
     try:
       self._initialize_camera()
       logging.info("Capturing image")
@@ -182,24 +201,27 @@ class pyChicken:
     try:
       # A live stream is running
       if self.running_livestream:
-        message = "Hey! We're running a livestream right now! Come check us out at %s" % self.youtube_feed_url
+        message = "Hey! We're running a livestream right now! Come check us out at foo"
 
         update = self.twitter.update_status(status=message)
 
-      else: 
+      else:
         message = self._get_tweet_fact()
-        if self._image_capture(): # This returns True if an image is captured
-          media_id = list()
-          logging.info("sending tweet with image")
-          media = self.twitter.media_upload(self.twitter_image)
-          logging.debug("uploading twitter media: %s", media.media_id_string)
-          media_id.append(media.media_id)
 
-          update = self.twitter.update_status(status=message, media_ids=media_id)
+      if self._image_capture():  # This returns True if an image is captured
+        media_id = list()
+        logging.info("sending tweet with image")
+        media = self.twitter.media_upload(self.twitter_image)
+        logging.debug("uploading twitter media: %s",
+            media.media_id_string)
+        media_id.append(media.media_id)
 
-        else:
-          logging.info("sending tweet without image")
-          update = self.twitter.update_status(status=message)
+        update = self.twitter.update_status(
+        status=message, media_ids=media_id)
+
+      else:
+        logging.info("sending tweet without image")
+        update = self.twitter.update_status(status=message)
 
       logging.info("Updated Twitter status: %s", update.id)
       return update.id
@@ -219,43 +241,16 @@ class pyChicken:
     fact_author = fact['source']
 
     if fact_type == "fact":
-      message = "Chicken fact %s: %s source: %s" % (fact_number, 
-      fact_content,
-      fact_author)
+      message = "Chicken fact %s: %s source: %s" % (fact_number,
+                  fact_content,
+                  fact_author)
 
     if fact_type == "quote":
-      message = "Chicken Quote %s: %s --%s" % (fact_number, 
-      fact_content, 
-      fact_author)
+      message = "Chicken Quote %s: %s --%s" % (fact_number,
+                fact_content,
+                fact_author)
 
     return message
-
-  def _send_livestream(self):
-    """ Starts a youtube live stream of the chicken yard and sends out a tweet to the youtube live link
-    """
-
-    try:
-      self.running_livestream = True
-      self._initialize_camera()
-      youtube_url = 'rtmp://a.rtmp.youtube.com/live2/'
-      stream_cmd = 'ffmpeg -f h264 -r 25 -i - -itsoffset 5.5 -fflags nobuffer -f alsa -ac 1 -i hw:0,1 -vcodec copy -acodec aac -ac 1 -ar 8000 -ab 32k -map 0:0 -map 1:0 -ss 30 -strict experimental -f flv %s%s' % (youtube_url, self.youtube_key)
-
-      stream_pipe = subprocess.Popen(stream_cmd, shell=True, 
-              stdin=subprocess.PIPE)
-
-      self.camera.start_recording(stream_pipe.stdin, format='h264', bitrate = 2000000)
-      while True:
-        self.camera.wait_recording(self.livestream_duration)
-      
-    except KeyboardInterrupt: 
-      self.camera.stop_recording()
-
-    finally:
-      logging.info("Closing down livestream")
-      self._close_camera()
-      stream_pipe.stdin.close()
-      self.running_livestream = False
-
 
   def _load_facts_file(self):
     """ Takes a CSV fie in the format:
@@ -278,7 +273,7 @@ class pyChicken:
 
     logging.info("Motion sensor event triggered")
     self._send_tweet()
-    
+
   def _run_motion_sensor(self):
     """The threaded motion sensor object"""
 
@@ -294,27 +289,115 @@ class pyChicken:
     logging.info("Starting Facts Thread")
     while True:
       sleep(3600)
-      self.facts, self.facts_count = self._load_facts_file()  
+      self.facts, self.facts_count = self._load_facts_file()
 
   def _run_livestream(self):
-    """The threaded livestream object"""
+    """ The thread function to run the livestream"""
 
-    logging.info("Starting livestream thread in 30 seconds")
-    self._send_livestream()
+    self.running_livestream = True
+    global output 
+    output = StreamingOutput()
+    self._initialize_camera()
+    logging.info("starting livestream")
+    self.camera.start_recording(output, format='mjpeg')
+    try:
+      address = ('', 8000)
+      server = StreamingServer(address, StreamingHandler)
+      server.serve_forever()
+      sleep(100)
+      logging.info("Closing livestream")
+      self._close_camera()
+      self.running_livestream = False
+
+    except Exception as e:
+      logging.error("Unable to begin livestream", exc_info=True)
+      raise e
 
   def run(self):
     """ The primary function. This is called by a script, loads a CSV file full of facts to use as social media content, and begins checking for the motion sensor, start livestreams, etc.
     """
 
-    facts_thread = threading.Thread(name='facts', 
-                  target=self._run_retrieve_facts)
+    facts_thread = threading.Thread(name='facts',
+            target=self._run_retrieve_facts)
     motion_thread = threading.Thread(name='motion_sensor',
-                  target=self._run_motion_sensor)
+            target=self._run_motion_sensor)
     livestream_thread = threading.Thread(name='livestream',
-                  target=self._run_livestream)
+              target=self._run_livestream)
 
     facts_thread.start()
-    livestream_thread.start()
-    sleep(45)
     motion_thread.start()
+    livestream_thread.start()
 
+
+class StreamingOutput(object):
+  def __init__(self):
+    self.frame = None
+    self.buffer = io.BytesIO()
+    self.condition = threading.Condition()
+
+  def write(self, buf):
+    if buf.startswith(b'\xff\xd8'):
+      # New frame, copy the existing buffer's content and notify all
+      # clients it's available
+      self.buffer.truncate()
+      with self.condition:
+        self.frame = self.buffer.getvalue()
+        self.condition.notify_all()
+      self.buffer.seek(0)
+    return self.buffer.write(buf)
+
+class StreamingHandler(server.BaseHTTPRequestHandler):
+  def __init__(self):
+    self.page = """\
+    < html >
+    <head >
+    <title > picamera MJPEG streaming demo < /title >
+    </head >
+    <body >
+    <h1 > PiCamera MJPEG Streaming Demo < /h1 >
+    <img src = "stream.mjpg" width = "640" height = "480" / >
+    </body >
+    </html >
+    """
+
+  def do_GET(self):
+    if self.path == '/':
+      self.send_response(301)
+      self.send_header('Location', '/index.html')
+      self.end_headers()
+    elif self.path == '/index.html':
+      content = self.page.encode('utf-8')
+      self.send_response(200)
+      self.send_header('Content-Type', 'text/html')
+      self.send_header('Content-Length', len(content))
+      self.end_headers()
+      self.wfile.write(content)
+    elif self.path == '/stream.mjpg':
+      self.send_response(200)
+      self.send_header('Age', 0)
+      self.send_header('Cache-Control', 'no-cache, private')
+      self.send_header('Pragma', 'no-cache')
+      self.send_header(
+      'Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+      self.end_headers()
+      try:
+        while True:
+          with output.condition:
+            output.condition.wait()
+            frame = output.frame
+          self.wfile.write(b'--FRAME\r\n')
+          self.send_header('Content-Type', 'image/jpeg')
+          self.send_header('Content-Length', len(frame))
+          self.end_headers()
+          self.wfile.write(frame)
+          self.wfile.write(b'\r\n')
+
+      except Exception as e:
+        logging.warning('Removed streaming client %s: %s', self.client_address, str(e))
+    else:
+      self.send_error(404)
+      self.end_headers()
+
+class StreamingServer(socketserver.ThreadingMixIn, server.HTTPServer):
+  allow_reuse_address = True
+  daemon_threads = True
